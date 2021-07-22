b907c37294343bfceb33b7cfe0a483066fe4c48c
Vivek Krishnan <cpkrish123@gmail.com>
2021-07-22 21:19:40 +0000

    added s3 bucket module

---
+++ s3-bucket.tf
@@ -0,0 +1,10 @@
+module "s3-bucket" {
+  source  = "terraform-aws-modules/s3-bucket/aws"
+  version = "2.6.0"
+  # insert the 5 required variables here
+  acceleration_status ="Enabled"
+  bucket = "hash-vivek-bucket"
+  bucket_prefix = "prefix"
+  policy  = ""
+  request_payer  = ""
+}   
