c5bcd4cf7365d14d9a7074dd2af1d8666c6ce69b
Vivek Krishnan <cpkrish123@gmail.com>
2021-07-23 00:11:52 +0000

    mod to s3 bucket module - err fix

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -3,8 +3,7 @@ module "s3-bucket" {
   version = "2.6.0"
   # insert the 5 required variables here
   acceleration_status ="Enabled"
-  var.bucket = "hash-vivek-bucket"
-  var.bucket_prefix = "prefix"
-  policy  = ""
-  request_payer  = ""
+  bucket = "hash-vivek-bucket"
+  bucket_prefix = "prefix"
+
 }   
